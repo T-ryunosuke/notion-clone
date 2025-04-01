96947934aa5ac38f8a05ce2d3c04cfca5d686f3d
Ryunosuke <dog.kira@yahoo.ne.jp>
2025-04-01 17:01:57 +0900

    ノート詳細を表示/更新する機能

--- src/components/NoteList/index.tsx
+++ src/components/NoteList/index.tsx
@@ -1,6 +1,11 @@
 import { cn } from '@/lib/utils';
 import { NoteItem } from './NoteItem';
 import { useNoteStore } from '@/modules/notes/note.state';
+import { useCurrentUserStore } from '@/modules/auth/current-user.state';
+import { useState } from 'react';
+import { noteRepository } from '@/modules/notes/note.repository';
+import { Note } from '@/modules/notes/note.entity';
+import { useNavigate } from 'react-router-dom';
 
 interface NoteListProps {
   layer?: number;
@@ -8,8 +13,36 @@ interface NoteListProps {
 }
 
 export function NoteList({ layer = 0, parentId }: NoteListProps) {
+  const navigate = useNavigate();
   const noteStore = useNoteStore();
   const notes = noteStore.getAll();
+  const { currentUser } = useCurrentUserStore();
+  const [expanded, setExpanded] = useState<Map<number, boolean>>(new Map());
+
+  const createChild = async (e: React.MouseEvent, parentId: number) => {
+    e.stopPropagation();
+    const newNote = await noteRepository.create(currentUser!.id, { parentId });
+    noteStore.set([newNote]);
+    setExpanded((prev) => prev.set(parentId, true));
+    moveToDetail(newNote.id);
+  };
+
+  const fetchChildren = async (e: React.MouseEvent, note: Note) => {
+    e.stopPropagation();
+    const children = await noteRepository.find(currentUser!.id, note.id);
+    if (children == null) return;
+    noteStore.set(children);
+    setExpanded((prev) => {
+      const newExpanded = new Map(prev);
+      newExpanded.set(note.id, !prev.get(note.id));
+      return newExpanded;
+    });
+  };
+
+  const moveToDetail = (noteId: number) => {
+    navigate(`/notes/${noteId}`);
+  };
+
   return (
     <>
       <p
@@ -21,10 +54,21 @@ export function NoteList({ layer = 0, parentId }: NoteListProps) {
       >
         ページがありません
       </p>
-      {notes.map((note) => {
+      {notes
+        .filter((note) => note.parent_document == parentId)
+        .map((note) => {
         return (
           <div key={note.id}>
-            <NoteItem note={note} layer={layer} />
+            <NoteItem
+              note={note}
+              layer={layer}
+              expanded={expanded.get(note.id)}
+              onClick={() => moveToDetail(note.id)}
+              onExpand={(e: React.MouseEvent) => fetchChildren(e, note)}
+              onCreate={(e) => createChild(e, note.id)} />
+              {expanded.get(note.id) && (
+                <NoteList layer={layer + 1} parentId={note.id} />
+              )}
           </div>
         );
       })}
